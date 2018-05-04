a0624720e1eaee0803685bac88e59eb952e1cff9
David Ritchey <ritchey0713@gmail.com>
2018-05-04 16:33:56 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,9 +6,9 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = __
+  movie = Movie.new
   movie.title = "This is a title."
-  __
+  movie.save
 end
 
 def can_be_created_with_a_hash_of_attributes
@@ -19,17 +19,17 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.create(attributes)
 end
 
-def can_be_created_in_a_block(args = __)
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date:1990 })
   Movie.create do |m|
-    __
+    m.update(args)
   end
 end
 
 def can_get_the_first_item_in_the_database
-  __
+  
 end
 
 def can_get_the_last_item_in_the_database
